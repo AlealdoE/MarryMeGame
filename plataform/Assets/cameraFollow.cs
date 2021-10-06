6d41aefec5d51ef9b8e8f853d81eea6964f4ca60
AlealdoE <92043141+AlealdoE@users.noreply.github.com>
2021-10-06 13:36:09 -0300

    update build

---
+++ plataform/Assets/cameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform Player;
+
+    private void Update()
+    {
+        transform.position = Vector2.Lerp(transform.position, Player.position,0.1f);
+    }
+}
